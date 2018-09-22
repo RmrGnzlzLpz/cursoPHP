bfd2f4471c07aaf5ca39e9a0aedcedf2dada93f8
RmrGnzlzLpz <rmrgnzlzlpz@gmail.com>
2018-09-21 23:46:27 -0500

    Estilos template

--- 03.MVC/views/template.php
+++ 03.MVC/views/template.php
@@ -6,6 +6,48 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Template</title>
+    <style>
+
+        header{
+            position:relative;
+            margin:auto;
+            text-align:center;
+            padding:5px;
+        }
+
+        nav{
+            position:relative;
+            margin:auto;
+            width:100%;
+            height:auto;
+            background:black;
+        }
+
+        nav ul{
+            position:relative;
+            margin:auto;
+            width:50%;
+            text-align: center;
+        }
+
+        nav ul li{
+            display:inline-block;
+            width:24%;
+            line-height: 50px;
+            list-style: none;
+        }
+
+        nav ul li a{
+            color:white;
+            text-decoration: none;
+        }
+
+        section{
+            position:relative;
+            padding:20px;
+        }
+
+    </style>
 </head>
 <body>
     <header>
@@ -24,7 +66,7 @@
         </ul>
     </nav>
     <section>
-        <h1>PÁGINA DE PRUEBA 2</h1>
+        <h1>PÁGINA DE INICIO</h1>
     </section>
 </body>
 </html>
